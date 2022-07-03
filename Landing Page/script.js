32aff6ef18eb7cffd4851d648b1ae5b75a305c8f
Moyinoluwa Adelowo <104604245+Moyinoluwa-10@users.noreply.github.com>
2022-07-03 18:11:16 +0100

    Update script.js

--- Landing Page/script.js
+++ Landing Page/script.js
@@ -1,34 +1,17 @@
 // Making Hamburger Menu Functional
 
 const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('.nav-menu');
-const blank = document.getElementById('blank')
+const navMenu = document.querySelector('.navlinks');
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
-    blank.style.display = 'block'
-    disappear()
+
 })
 
 document.querySelectorAll("nav-link").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
-    blank.style.display = 'none'
 
 }))
 
-
-function disappear() {
-    hamburger.addEventListener("click", () => {
-        blank.style.display = 'none'
-        show() 
-    })
-}
-
-function show() {
-    hamburger.addEventListener("click", () => {
-        blank.style.display = 'block'
-        disappear()
-    })
-}
