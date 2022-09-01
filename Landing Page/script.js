ddc6bf2d3fee61a189094f8a3c525135f89d839f
Moyinoluwa10 <moyinadelowo@gmail.com>
2022-09-01 23:57:50 +0100

    Updated index.html

--- Landing Page/script.js
+++ Landing Page/script.js
@@ -6,12 +6,23 @@ const navMenu = document.querySelector('.navlinks');
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
-
 })
 
 document.querySelectorAll("nav-link").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
-
 }))
 
+
+// Adding Light and Dark Mode Functionality
+
+const dark = document.getElementById("circle2");
+const light = document.getElementById("circle1");
+
+dark.addEventListener("click", () => {
+    document.body.classList.add("dark");
+})
+
+light.addEventListener("click", () => {
+    document.body.classList.remove("dark");
+})
